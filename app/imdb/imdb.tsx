eb229915d4641e1b0b690e805bc7b6c7a48afff1
mzsellem <marleysellem@gmail.com>
2023-09-15 13:44:00 -0400

    establish routes: imdb and home

---
+++ app/imdb/imdb.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function ShowMovies() {
+   return (
+      <>
+         <Link href="/">HOME</Link>
+         <h1>client</h1>
+      </>
+   );
+}
---
+++ app/imdb/page.tsx
@@ -0,0 +1,10 @@
+import Imdb from './imdb'
+
+export default function GetMovies() {
+   return (
+      <>
+         <h1>SERVER</h1>
+         <Imdb/>
+      </>
+   );
+}
