bde39bcbc742ce0f771516ee39d6263f644aae98
mzsellem <marleysellem@gmail.com>
2023-09-15 16:24:36 -0400

    successful api connection

--- app/imdb/imdb.tsx
+++ app/imdb/imdb.tsx
@@ -1,13 +1,45 @@
+'use client'
 import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
-export default function ShowMovies() {
+export default function ShowFacts() {
+   const [catFacts, setCatFacts] = useState([]);
+   const [loading, setLoading] = useState(true);
+ 
+   useEffect(() => {
+     async function fetchCatFacts() {
+       try {
+         const response = await fetch('https://cat-fact.herokuapp.com/facts');
+         if (!response.ok) {
+           throw new Error('Network response was not ok');
+         }
+         const data = await response.json();
+         setCatFacts(data);
+         setLoading(false);
+       } catch (error) {
+         console.error('Error fetching cat facts:', error);
+       }
+     }
+ 
+     fetchCatFacts();
+   }, []);
    return (
       <>
          <Link href="/">HOME</Link>
          <div className="font-bold m-6">
-            <h1>MOVIE LIST</h1>
+            <h1>CAT FACT LIST</h1>
          </div>
-      
+         <div>
+      {loading ? (
+        <p>Loading cat facts...</p>
+      ) : (
+        <ul>
+          {catFacts.map((fact) => (
+            <li key={fact._id}>{fact.text}</li>
+          ))}
+        </ul>
+      )}
+    </div>
       </>
    );
 }
